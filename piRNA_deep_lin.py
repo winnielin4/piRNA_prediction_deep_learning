f48c03c4f6d6ff9acc03c8f44ed13491a189a5a1
Weiran Lin <waynelin4wr@gmail.com>
2018-04-09 17:04:16 +0800

    Call external cnn model from cnn_lin_model; Optimize piRNA_deep_lin code

--- piRNA_deep_lin.py
+++ piRNA_deep_lin.py
@@ -7,17 +7,18 @@ from __future__ import print_function
 import argparse
 import sys
 import tempfile
+import numpy
 
 import piRNA_datasets as input_data
+import cnn_lin_model
 
 import tensorflow as tf
 
+
 FLAGS = None
 
-# dropout probability
-# pkeep = tf.placeholder(tf.float32)
-# pkeep_conv = tf.placeholder(tf.float32)
-
+LOGS_DIRECTORY = "logs/train"
+TOTAL_BATCH = 100000
 
 def deepnn(x):
 
@@ -145,38 +146,61 @@ def main(_):
     y_ = tf.placeholder(tf.float32, [None, 2])
 
     # Build the graph for the deep net
-    y_conv, keep_prob = deepnn(x)
+    # y_conv, keep_prob = deepnn(x)
+    
+    y_conv, keep_prob = cnn_lin_model.CNN(x)
+
 
     with tf.name_scope('loss'):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
     cross_entropy = tf.reduce_mean(cross_entropy)
+    
+    # Create a summary to monitor loss tensor
+    tf.summary.scalar('loss', cross_entropy)
+
 
     with tf.name_scope('adam_optimizer'):
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    
+
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
         correct_prediction = tf.cast(correct_prediction, tf.float32)
     accuracy = tf.reduce_mean(correct_prediction)
 
-    # graph_location = tempfile.mkdtemp()
-    # print('Saving graph to: %s' % graph_location)
-    # train_writer = tf.summary.FileWriter(graph_location)
-    # train_writer.add_graph(tf.get_default_graph())
+    # create a summary to monitor accuracy tensor
+    tf.summary.scalar('acc', accuracy)
+
+    # Merge all summaries into a single op
+    merged_summary_op = tf.summary.merge_all()
+
+    # Add ops to save and restore all the variables
+    saver = tf.train.Saver()
+    sess = tf.InteractiveSession()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(100000):
+
+        summary_writer = tf.summary.FileWriter(LOGS_DIRECTORY, graph=tf.get_default_graph())
+
+        max_acc = 0;
+
+        for i in range(TOTAL_BATCH):
             batch = piRNA.train.next_batch(50)
-            if i % 50 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-                print('step %d, training accuracy %g' % (i, train_accuracy))
-            train_step.run(feed_dict={x:batch[0], y_: batch[1], keep_prob: 0.5})
             
+            step, training_accuracy, summary = sess.run([train_step, accuracy, merged_summary_op], feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})            
+            
+            # Write logs at every iteration
+            summary_writer.add_summary(summary, i)
+            
+            # print out results
+            if i % 50 == 0:
+                print('step %d, training accuracy %g' % (i, training_accuracy))
             if i % 1000 == 0:
                 print('test accuracy %g' % accuracy.eval(feed_dict={x: piRNA.test.images, y_:piRNA.test.labels, keep_prob: 1.0}))
         
+        # Test Set
         print('test accuracy %g' % accuracy.eval(feed_dict={x: piRNA.test.images, y_:piRNA.test.labels, keep_prob: 1.0}))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
