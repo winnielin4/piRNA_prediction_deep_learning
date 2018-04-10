97843b672e570d0340a9b57b5cdd8f8a261d626b
Weiran Lin <waynelin4wr@gmail.com>
2018-04-10 13:38:18 +0800

    Try many combination of arguments in CNN. (Not vital changes)

--- cnn_lin_model.py
+++ cnn_lin_model.py
@@ -8,23 +8,25 @@ import tensorflow.contrib.slim as slim
 
 # Create model of CNN with slim api
 def CNN(inputs, is_training=True):
-    batch_norm_params = {'is_training': is_training, 'decay': 0.9, 'updates_collections': None}
+    batch_norm_params = {'is_training': is_training, 'decay': 0.999, 'updates_collections': None}
     with slim.arg_scope([slim.conv2d, slim.fully_connected],
                         normalizer_fn=slim.batch_norm,
                         normalizer_params=batch_norm_params):
-        x = tf.reshape(inputs, [-1, 1, 35,5])
+        x = tf.reshape(inputs, [-1, 1, 35, 5])
 
         # For slim.conv2d, default argument values are like
         # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
         # padding='SAME', activation_fn=nn.relu,
         # weights_initializer = initializers.xavier_initializer(),
         # biases_initializer = init_ops.zeros_initializer,
-        net = slim.conv2d(x, 32, [1, 16], scope='conv1')
+        net = slim.conv2d(x, 8, [1, 2], scope='conv1')
+        # net = slim.max_pool2d(net, [1, 2], scope='pool1')        
+        net = slim.conv2d(net, 16, [1, 2], scope='conv2')
         net = slim.max_pool2d(net, [1, 2], scope='pool1')
-        net = slim.conv2d(net, 64, [1, 16], scope='conv2')
+        # net = slim.max_pool2d(net, [1, 2], scope='pool2')
+        net = slim.conv2d(net, 32, [1, 2], scope='conv3')
+        net = slim.conv2d(net, 64, [1, 2], scope='conv4')
         net = slim.max_pool2d(net, [1, 2], scope='pool2')
-        net = slim.conv2d(net, 256, [1, 16], scope='conv3')
-        net = slim.max_pool2d(net, [1, 2], scope='pool3')
         net = slim.flatten(net, scope='flatten3')
 
         # For slim.fully_connected, default argument values are like
@@ -32,9 +34,9 @@ def CNN(inputs, is_training=True):
         # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
         # weights_initializer = initializers.xavier_initializer(),
         # biases_initializer = init_ops.zeros_initializer,
-        net = slim.fully_connected(net, 256, scope='fc3')
+        net = slim.fully_connected(net, 64,normalizer_fn=None, scope='fc3')
         net = slim.dropout(net, is_training=is_training, scope='dropout3')  # 0.5 by default
-        outputs = slim.fully_connected(net, 2, activation_fn=None, normalizer_fn=None, scope='fco')
+        outputs = slim.fully_connected(net, 2, activation_fn=None,normalizer_fn=None, scope='fco')
 
         keep_prob = tf.placeholder(tf.float32)
     return outputs, keep_prob
