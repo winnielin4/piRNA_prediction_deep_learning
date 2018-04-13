c036f8965999d7eba21f835b70abc05a0a1ace15
Weiran Lin <waynelin4wr@gmail.com>
2018-04-13 14:16:42 +0800

    Update cross validation pytho file

--- cross_validation.py
+++ cross_validation.py
@@ -4,3 +4,10 @@ def acc(sum_acc, this_fold_accuracy, fold):
     print('5-CV Average Validation Set Accuracy %g' % (sum_acc/(fold+1)))
     print('---------------------------------------------')
     return sum_acc
+
+def auc(sum_auc, this_fold_auc, fold):
+    print('---------------------------------------------')
+    sum_auc += this_fold_auc
+    print('5-CV Average Validation set AUC %g' % (sum_auc/(fold+1)))
+    print('---------------------------------------------')
+    return sum_auc
